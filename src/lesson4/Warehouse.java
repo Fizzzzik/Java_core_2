436e362326e5f92b7bd2928975d678142e12e212
Fizzzzik <fi4zik@gmail.com>
2022-07-21 01:06:05 +0300

    HomeworkTask2. Решил ООП базовый

---
+++ src/lesson4/Warehouse.java
@@ -0,0 +1,40 @@
+package lesson4;
+
+public class Warehouse {
+    //статический атрибут
+    public static int count = 0;
+    //атрибут
+    private String position;
+    private int quantity;
+
+    //Конструктор экземпляра класса
+    public Warehouse(String positionName, int quantityPieces){
+        this.position = positionName;
+        this.quantity = quantityPieces;
+        count++;
+    }
+
+
+    //Метод экземпляра класса
+    public void takePosition() {
+        this.quantity = quantity - 1;
+        //System.out.println("Осталось товара: " + position + " " + quantity + " штук");
+
+    }
+
+
+    //Геттеры
+    public String getPosition() {
+        return position;
+    }
+    public int getQuantityPieces() {
+        return quantity;
+    }
+    //Сеттеры
+    public void setPosition(int vodka) {
+        this.position = position;
+    }
+    public void setQuantityPieces(int newQuantity) {
+        this.quantity = newQuantity;
+    }
+}
---
+++ src/lesson4/Workers.java
@@ -0,0 +1,53 @@
+package lesson4;
+
+public class Workers {
+    //статический атрибут
+    public static int count = 0;
+    //атрибут
+    private String name;
+    private int age;
+    private int defective;
+
+    //Конструктор экземпляра класса
+    public Workers(String workerName, int workerAge, int workerDefective){
+        this.name = workerName;
+        this.age = workerAge;
+        this.defective = workerDefective;
+        count++;
+    }
+
+
+    //Метод экземпляра класса
+    public void takeDefective() {
+        this.defective = defective + 1;
+        System.out.println(this.name + ": Ура! Я испортил водку!");
+        //System.out.println(this.defective + ": всего");
+    }
+
+  //  public void sayHura() {
+      //  System.out.println(this.name + ": Ура! Я испортил водку!");
+        //this.defective = defective + 1;
+       // System.out.println(this.defective + ": всего");
+    //}
+
+    //Геттеры
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public int getDefective() {
+        return defective;
+    }
+    //Сеттеры
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setDefective(int defective) {
+        this.defective = defective;
+    }
+}
